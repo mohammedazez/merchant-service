6af2ab2049c5926780ac9ea7c8545619c73381e8
mohammedazez <muhamadaziz047@gmail.com>
2021-11-05 07:23:55 +0700

    add: formatter

---
+++ layer/user/formatter.go
@@ -0,0 +1,39 @@
+package user
+
+import (
+	"merchant-service/entity"
+	"time"
+)
+
+type UserFormat struct {
+	ID       string `json:"id"`
+	FullName string `json:"full_name"`
+	Email    string `json:"email"`
+	Role     string `json:"role"`
+	Outlet   []entity.Outlet
+}
+
+type UserDeleteFormat struct {
+	Message    string    `json:"message"`
+	TimeDelete time.Time `json:"time_delete"`
+}
+
+func Format(user entity.User) UserFormat {
+	var formatUser = UserFormat{
+		ID:       user.ID,
+		FullName: user.FullName,
+		Email:    user.Email,
+		Outlet:   user.Outlet,
+	}
+
+	return formatUser
+}
+
+func FormatDelete(msg string) UserDeleteFormat {
+	var deleteFormat = UserDeleteFormat{
+		Message:    msg,
+		TimeDelete: time.Now(),
+	}
+
+	return deleteFormat
+}
